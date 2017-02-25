2173ee3bc7b0c78925814821ba6ef7dea364437f
Brandon Fergerson <brandon.fergerson@codebrig.com>
2017-02-25 18:00:39 -0500

    ver. 1.1/Alpha

--- src/main/groovy/com/codebrig/jnomad/JNomadCLI.groovy
+++ src/main/groovy/com/codebrig/jnomad/JNomadCLI.groovy
@@ -24,8 +24,8 @@ import java.util.concurrent.TimeUnit
  */
 class JNomadCLI {
 
-    public static final String JNOMAD_VERSION = "1.0/Alpha"
-    public static final String JNOMAD_BUILD_DATE = "2017.01.31"
+    public static final String JNOMAD_VERSION = "1.1/Alpha"
+    public static final String JNOMAD_BUILD_DATE = "2017.02.25"
 
     @Parameter(names = ["-f", "-log_file"], description = "Log console output to specified file")
     public String logFile
